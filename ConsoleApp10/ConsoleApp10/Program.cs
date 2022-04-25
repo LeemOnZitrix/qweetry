d90fd7e19eefd86cb8f576b0cea076bf13ae3e5c
dasad44 <dasad33_79@o2.pl>
2022-04-25 21:23:55 +0200

    cos

--- ConsoleApp10/ConsoleApp10/Program.cs
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -10,19 +10,21 @@ namespace ConsoleApp10
     {
         static void Main(string[] args)
         {
-            int n = 0;
+            int n = 11;
             if(n == 0)
             {
-                Console.WriteLine();
+                Console.WriteLine("3211432");
                 return;
             }
             else
             {
-                Console.WriteLine("dsad");
+                Console.WriteLine("ds3123211312312ad");
             }
             for(int i = 0; i < n; i++)
             {
-                Console.WriteLine("adasdadas");
+                Console.WriteLine("3213412412421");
+                Console.WriteLine("32131213");
+                Console.WriteLine("12324rt25rtg");
             }
         }
     }
